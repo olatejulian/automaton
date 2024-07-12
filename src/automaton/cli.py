067ae3c2334c5fce0f0e27eb9af0359e304d8d0d
olatejulian <julian.lf.olate@gmail.com>
2024-07-12 15:30:39 -0300

    feat: :sparkles: add logging and exception handle

--- src/automaton/cli.py
+++ src/automaton/cli.py
@@ -1,30 +1,41 @@
+import logging
+import sys
+
 from typer import Typer
 
+from .series import get_season_episode, is_video_file, video_name_format
 from .utils import (
     directory_name,
     get_file_extension,
     list_filenames,
     rename_file,
 )
-from .series import get_season_episode, is_video_file, video_name_format
+
+logger = logging.getLogger()
 
 cli = Typer()
 
 
 @cli.command()
 def rename_series_episodes(path: str) -> None:
-    filenames = list_filenames(path)
+    try:
+        filenames = list_filenames(path)
 
-    for filename in filenames:
-        if is_video_file(filename):
-            if season_episode := get_season_episode(filename):
-                new_name = video_name_format(
-                    directory_name(path),
-                    season_episode,
-                    get_file_extension(filename),
-                )
+        for filename in filenames:
+            if is_video_file(filename):
+                if season_episode := get_season_episode(filename):
+                    new_name = video_name_format(
+                        directory_name(path),
+                        season_episode,
+                        get_file_extension(filename),
+                    )
 
-                rename_file(path, filename, new_name)
+                    rename_file(path, filename, new_name)
+
+    except Exception as e:
+        logger.exception(e, stack_info=True)
+
+        sys.exit(1)
 
 
 @cli.command()
