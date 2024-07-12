ae109d15d8caf8fe7446bdb4644efc379750c545
olatejulian <julian.lf.olate@gmail.com>
2024-07-11 21:43:03 -0300

    refactor: :recycle: make reader generic

--- src/automaton/read_url_file.py
+++ src/automaton/read_url_file.py
@@ -1,49 +1,24 @@
-import validators
+from typing import Callable, Iterator, TypeVar
 
-from .file_verify import file_verify
-
-
-class Lines:
-    def __init__(self, urls: list[str], not_urls: list[str]):
-        self.__urls = urls
-        self.__not_urls = not_urls
-
-    def urls_count(self) -> int:
-        return len(self.urls)
-
-    def not_urls_count(self) -> int:
-        return len(self.not_urls)
-
-    @property
-    def urls(self) -> list[str]:
-        return self.__urls
-
-    @property
-    def not_urls(self) -> list[str]:
-        return self.__not_urls
+T = TypeVar("T")
 
 
-def read_plaintext_url_file(file_path: str) -> Lines:
-    if not file_verify(file_path):
-        raise FileNotFoundError
+class Text:
+    def __init__(self, text_file_path: str):
+        self.__text_path = text_file_path
 
-    with open(file_path, "r", encoding="utf-8") as file:
-        urls = []
+    def __iter__(self):
+        with open(
+            file=self.__text_path, mode="r", encoding="utf-8"
+        ) as text_file:
+            while line := text_file.readline():
+                yield line
 
-        not_urls = []
+    def map(self, func: Callable[[str], T]) -> Iterator[T]:
+        for line in self:
+            yield func(line)
 
-        while True:
-            if line := file.readline():
-
-                striped_line = line.strip()
-
-                if validators.url(striped_line):
-                    urls.append(striped_line)
-
-                else:
-                    not_urls.append(line)
-
-            else:
-                break
-
-        return Lines(urls, not_urls)
+    def filter(self, func: Callable[[str], bool]) -> Iterator[str]:
+        for line in self:
+            if func(line):
+                yield line
