c9ad73cf3b36178c76e9727861985e2765ead1f0
olatejulian <julian.lf.olate@gmail.com>
2024-08-06 00:08:34 -0300

    refactor: :recycle: add main function in client/server example

--- src/automaton/apps/rpc/example/client.py
+++ src/automaton/apps/rpc/example/client.py
@@ -1,20 +1,36 @@
+import sys
 import time
 from datetime import datetime
 
 from automaton.modules.rpc import RpcClient
 
-if __name__ == "__main__":
-    app = RpcClient("127.0.0.1", 8086)
+
+def main():
+    app = RpcClient("127.0.0.1", 8088)
 
     app.connect()
-    print("Client connected")
 
     while True:
-        print("Requesting message...")
-        response = app.message_handler_class(
-            f"hello, it is{datetime.now().strftime(" %Y-%m-%d %H:%M:%S")}"
-        )
+        try:
+            response = app.message_handler_class(
+                f"hello, it is{datetime.now().strftime(" %Y-%m-%d %H:%M:%S")}"
+            )
 
-        print(response.data)
+            print("Message Handler Class Response Data:", response.data)
 
-        time.sleep(2)
+            response = app.message_handler_function(
+                f"hello, it is{datetime.now().strftime(" %Y-%m-%d %H:%M:%S")}"
+            )
+
+            print("Message Handler Function Response Data:", response.data)
+
+            time.sleep(2)
+
+        except KeyboardInterrupt:
+            break
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- src/automaton/apps/rpc/example/server.py
+++ src/automaton/apps/rpc/example/server.py
@@ -1,11 +1,22 @@
+import sys
+
 from automaton.apps.rpc.example import handler
 from automaton.modules.rpc import RpcServer
 
-if __name__ == "__main__":
-    app = RpcServer("127.0.0.1", 8086)
+
+def main():
+    app = RpcServer("127.0.0.1", 8088)
 
     app.register_handler(handler.MessageHandlerClass())
 
-    app.register_handler(handler.message_handler_function)
+    app.register_handler(
+        handler.message_handler_function, name="message_handler_function"
+    )
 
     app.run()
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
